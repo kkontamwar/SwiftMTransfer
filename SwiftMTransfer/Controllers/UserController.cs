d1b55a1802ef025fb0c807d218fcda6561ee8c28
kaustubh kontamwar <kkontamwar@hotmail.com>
2019-03-20 12:22:02 +0530

    Chnages to Test CI CD

--- SwiftMTransfer/Controllers/UserController.cs
+++ SwiftMTransfer/Controllers/UserController.cs
@@ -76,7 +76,7 @@ namespace SwiftMTransfer.Controllers
 		[Route("api/User/TestUserControl")]
 		public string TestUserControl()
 		{
-			return "Hi from Ravi";
+			return "Eureka!!!";
 		}
 
 		/// <summary>
